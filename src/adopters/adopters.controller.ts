90866808da6ef1b8034cf1cd4bb7dd1320f254f6
BlakeFernandes <blakef148@gmail.com>
2025-02-25 13:59:20 +1300

    refactor: post adopters comply wth openapi spec (puppyId param -> body)

--- src/adopters/adopters.controller.ts
+++ src/adopters/adopters.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Param,
   Post,
   UsePipes,
   ValidationPipe,
@@ -11,21 +10,29 @@ import { Puppy } from 'src/database/puppies.dto';
 import { AdoptersService } from './adopters.service';
 
 export class AdoptDto {
-  @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Puppy ID should be a string' })
+  @IsNotEmpty({ message: 'Puppy ID cannot be empty' })
+  @Matches(/\S/, { message: 'Puppy ID cannot contain only spaces' })
+  id: string;
+
+  @IsString({ message: 'Name should be a string' })
+  @IsNotEmpty({ message: 'Name cannot be empty' })
   @Matches(/\S/, { message: 'Name cannot contain only spaces' })
   name: string;
 
-  @IsEmail({}, { message: 'Email is not valid' })
+  @IsEmail({}, { message: 'Email should be a string' })
+  @IsNotEmpty({ message: 'Email cannot be empty' })
+  @Matches(/\S/, { message: 'Email cannot contain only spaces' })
   email: string;
 
-  @IsString({ message: 'Phone number is not valid' })
+  @IsString({ message: 'Phone should be a string' })
   @IsNotEmpty({ message: 'Phone cannot be empty' })
-  @Matches(/\S/, { message: 'Key cannot contain only spaces' })
+  @Matches(/\S/, { message: 'Phone cannot contain only spaces' })
   phone: string;
 
-  @IsString({ message: 'Message is not valid' })
-  @IsNotEmpty({ message: 'Message is required' })
-  @Matches(/\S/, { message: 'Key cannot contain only spaces' })
+  @IsString({ message: 'Message should be a string' })
+  @IsNotEmpty({ message: 'Message cannot be empty' })
+  @Matches(/\S/, { message: 'Message cannot contain only spaces' })
   message: string;
 }
 
@@ -41,14 +48,11 @@ export class AdoptersController {
    * @returns The puppy that was adopted or null if the puppy was not found {@link Puppy}
    * @throws NotFoundException if the puppy is not found
    */
-  @Post(':id')
+  @Post()
   @UsePipes(new ValidationPipe({ transform: true }))
-  findAll(
-    @Param('id') id: string,
-    @Body() query: AdoptDto,
-  ): Promise<Puppy | null> {
+  findAll(@Body() query: AdoptDto): Promise<Puppy | null> {
     return this.adoptService.adopt({
-      id: id,
+      id: query.id,
       name: query.name,
       email: query.email,
       phone: query.phone,
